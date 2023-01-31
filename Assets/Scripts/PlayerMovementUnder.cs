31536890c21730171e841e56010ee799fdc8d2ab
CabeMax1 <C21486482@mytudublin.ie>
2023-01-31 13:22:08 +0000

    playermovement maxspeed set

--- Assets/Scripts/PlayerMovementUnder.cs
+++ Assets/Scripts/PlayerMovementUnder.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovementUnder : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 5.0f;
-
+    public float maxVelocity = 5;
     private Rigidbody2D _playerRigidbody;
     private void Start()
     {
@@ -18,7 +18,7 @@ public class PlayerMovementUnder : MonoBehaviour
     private void Update()
     {
         MovePlayer();
-
+        _playerRigidbody.velocity = Vector2.ClampMagnitude(_playerRigidbody.velocity, maxVelocity);
     }
     private void MovePlayer()
     {
