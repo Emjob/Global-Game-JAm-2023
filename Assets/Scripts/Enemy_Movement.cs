c5d0a8128f69cc93387b7c73bc704ab656a277fe
Emjob <99894739+Emjob@users.noreply.github.com>
2023-02-01 11:30:40 +0000

    Enemy Movement 1.0

---
+++ Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Enemy_Movement : MonoBehaviour
+{
+    public GameObject Player2;
+    public GameObject Player1;
+
+    public float distanceToPlayer2;
+    public float distanceToPlayer1;
+
+     private Transform targetPlayer;
+
+    public float speed;
+
+ //   public Transform playerDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  playerDistance = GameObject.FindGameObjectWithTag("Player2").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer2 = Vector3.Distance(transform.position, Player2.transform.position);
+        distanceToPlayer1 = Vector3.Distance(transform.position, Player1.transform.position);
+        Debug.Log(Mathf.Min(distanceToPlayer2, distanceToPlayer1));
+        if(distanceToPlayer2 < distanceToPlayer1)
+        {
+           targetPlayer = GameObject.FindGameObjectWithTag("Player2").GetComponent<Transform>();
+            transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+        }
+        if (distanceToPlayer1 < distanceToPlayer2)
+        {
+            targetPlayer = GameObject.FindGameObjectWithTag("Player1").GetComponent<Transform>();
+            transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+        }
+    }
+}
