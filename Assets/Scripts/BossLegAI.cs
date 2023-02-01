31d8cf5f8798b56a67dc2d845aac081b558ac9e4
Mixer-one <99894586+Mixer-one@users.noreply.github.com>
2023-02-01 00:25:15 +0000

    boss progress

--- Assets/Scripts/BossLegAI.cs
+++ Assets/Scripts/BossLegAI.cs
@@ -9,27 +9,77 @@ public class BossLegAI : MonoBehaviour
     public Vector2 speed;
     public float PlayerPos;
     private float BossPos;
+    private bool slamming;
+    private Vector2 starties;
+   public Vector2 endies = new Vector2(0,0);
+   public float timetaken;
+   public float timereturned;
+   private float timepassed;
+   [SerializeField] private AnimationCurve curve;
+   [SerializeField] private AnimationCurve reverseCurve;
 
     void Update()
     {
-
+        
     }
   
 
-
+    
      void OnTriggerStay2D (Collider2D other)
      {
         PlayerPos = other.gameObject.transform.position.x;
         BossPos = this.gameObject.transform.position.x;
+    if(!slamming)
+    {
+        StartCoroutine (Slam());
+        slamming = true;
+        endies[0] = BossPos;
+        starties[0] = BossPos;
 
-    if(PlayerPos>BossPos)
-    {
-        transform.Translate(speed * Time.deltaTime);  
+        if(PlayerPos>BossPos)
+        {
+            transform.Translate(speed * Time.deltaTime);  
+        }
+        else
+        {
+            transform.Translate((speed * Time.deltaTime)*-1);
+        }
+
     }
-    else
-    {
-        transform.Translate((speed * Time.deltaTime)*-1);
-    }
+     
+
+     IEnumerator Slam()
+     {
+       yield return new WaitForSeconds(2);
+        LeftField.SetActive(false);
+        RightField.SetActive(false);
+
+        timepassed = 0;
+        while (timepassed<timetaken)
+        {
+            
+            timepassed += Time.deltaTime;
+            float percentcomplete = Mathf.Clamp(timepassed / timetaken,0,1);
+            transform.position = Vector3.Lerp(starties,endies,curve.Evaluate(percentcomplete));
+            yield return null;
+        }
+        timepassed = 0;
+        while (timepassed<timetaken)
+        {
+            
+            timepassed += Time.deltaTime;
+            float percentundone = Mathf.Clamp(timepassed / timetaken,0,1);
+            transform.position = Vector3.Lerp(endies,starties,reverseCurve.Evaluate(percentundone));
+            yield return null;
+        }
+
+
+        yield return new WaitForSeconds(2);
+        slamming = false;
+        LeftField.SetActive(true);
+        RightField.SetActive(true);
+
      }
  
+}
 }
