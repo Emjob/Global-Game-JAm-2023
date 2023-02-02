f660dbd31a287fd39edce4ea2693e14e2035aaca
Mixer-one <99894586+Mixer-one@users.noreply.github.com>
2023-02-02 14:34:43 +0000

    cutscene script

--- Assets/Scripts/CutScenes.cs
+++ Assets/Scripts/CutScenes.cs
@@ -1,20 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutScenes : MonoBehaviour
 {
     public GameObject Panel1;
-    public GameObject Panel2;
-    public GameObject Panel3;
-    public GameObject Panel4;
-    public GameObject Panel5;
-    public GameObject Panel6;
+
 
     public Color MyColor;
     public Renderer MyRenderer;
+    private bool Fadein = true;
 
-    private float Alpha;
+    private float Alpha =0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,34 +25,38 @@ public class CutScenes : MonoBehaviour
         MyRenderer = Panel1.GetComponent<Renderer>();
         MyColor = new Color(1,1,1,Alpha/100);
         MyRenderer.material.color = MyColor;
-        Debug.Log(Alpha);
-        while(Alpha<100)
-        {
-        Alpha = Alpha+ 5/3;
-        }
+        StartCoroutine(SceneCut());
+        //Debug.Log(Alpha);
         if(Alpha >= 100)
         {
-        StartCoroutine(SceneCut());
+            Fadein = false;
+            
         }
-        while(Alpha>0)
+                if(Fadein)
         {
         Alpha = Alpha+ 5/3;
-        }
-        if(Alpha<=0)
-        {
-            StartCoroutine(SceneCut());
-        }
-
-
-
-
         
+        }
+
 
     }
     IEnumerator SceneCut()
    {
-        
         yield return new WaitForSeconds(2);
+                if(Alpha<=0)
+        {
+            
+        }
+                if(!Fadein)
+        {
+        Alpha = Alpha- 5/3;
+       
+        }
+
+        yield return new WaitForSeconds(2);
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+        Debug.Log("moving on");
 
 
    }
