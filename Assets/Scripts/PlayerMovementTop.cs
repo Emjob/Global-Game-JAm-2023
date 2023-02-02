eba99808a2b4db26ab12f4b4b4d54e9af4f3d02f
CabeMax1 <C21486482@mytudublin.ie>
2023-02-02 16:28:09 +0000

    Revert "jump sound work"
    This reverts commit 638b861033dcb1210186c1010b53df836c77fe4d.

--- Assets/Scripts/PlayerMovementTop.cs
+++ Assets/Scripts/PlayerMovementTop.cs
@@ -11,8 +11,6 @@ public class PlayerMovementTop : MonoBehaviour
     public GameObject grasshopperG;
     public Collider2D groundCheck;
 
-    public AudioSource jump;
-
     public bool jumping;
     public bool falling;
 
@@ -39,14 +37,12 @@ public class PlayerMovementTop : MonoBehaviour
         {
             jumping = true;
             falling = false;
-            jump.enabled = true;
             grasshopperG.GetComponent<Animator>().Play("GrassHopperJump");
         }
         if (_playerRigidbody.velocity.y < -0.25 && groundCheck == true)
         {
             jumping = false;
             falling = true;
-            jump.enabled = false;
             grasshopperG.GetComponent<Animator>().Play("GrassHopperFall");
         }
         if (_playerRigidbody.velocity.y > -0.1 && _playerRigidbody.velocity.y < 0.1)
